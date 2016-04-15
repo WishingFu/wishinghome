31af8489ba364e006863a2b069678ce4da06a91e
WishingFu <fuwenxing429@126.com>
2016-04-15 17:59:56 +0800

    1

--- wishinghome/src/utils/BinaryTree.java
+++ wishinghome/src/utils/BinaryTree.java
@@ -5,6 +5,33 @@ import java.util.List;
 
 public class BinaryTree<T extends Comparable<T>> {
 	
+	class BinaryTreeElement<T extends Comparable<T>>{
+
+		public BinaryTreeElement<T> parent;
+		public BinaryTreeElement<T> left;
+		public BinaryTreeElement<T> right;
+		public Comparable<T> t;
+		
+		public BinaryTreeElement(Comparable<T> t) {
+			this.wrap(t);
+		}
+		
+		public BinaryTreeElement<T> wrap(Comparable<T> t) {
+			this.t = t;
+			return this;
+		}
+		
+		public Comparable<T> unwrap(BinaryTreeElement<T> t) {
+			return this.t;
+		}
+
+		public int compareTo(BinaryTreeElement<T> o) {
+			return this.t.compareTo((T) o.t);
+		}
+	}
+
+	
+	
 	private BinaryTreeElement<T> root;
 	private List<BinaryTreeElement<T>> elements = new ArrayList<>();
 	
