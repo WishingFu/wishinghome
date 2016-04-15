cb009859e8914faa02ea3378dcca71e5916a85a5
WishingFu <fuwenxing429@126.com>
2016-04-15 11:22:27 +0800

    ball

--- wishinghome/node/srcs/effect.js
+++ wishinghome/node/srcs/effect.js
@@ -48,7 +48,7 @@ $(document).ready(function() {
 	setInterval(function() {
 		mc.clearRect(0, 0, mcanvas.width, mcanvas.height);
 		something(mc);
-	}, 10);
+	}, 5);
 });
 
 function openModalTest() {	
@@ -61,12 +61,38 @@ function restartSanke() {
 
 function something(mc) {
 	var m = mc.canvas;
+	var t = (new Date().getMilliseconds()) / 1000 * Math.PI ;
+	var s = - 1 / 2 * Math.cos( 2 * t );
+	var o = s * Math.PI / 2.5;
+	var ac = Math.asin(15 / 200);
+	var abs = Math.abs(o - Math.PI / 2);
+	// drawStaticBall(mc, ac);
+	drawMovingBall(mc, o);
+}
+
+function drawStaticBall(mc, ac) {
+	var m = mc.canvas;
 	mc.save();
 	mc.translate(m.width / 2, 100);
-	var t = (new Date().getMilliseconds()) / 1000 * Math.PI ;
-	var s = - Math.PI / 5 * Math.cos( 2 * t );
-	var o = s;
+	mc.rotate(ac);
+	drawBall(mc);
+	mc.restore();
+	mc.save();
+	mc.translate(m.width / 2, 100);
+	mc.rotate(-ac);
+	drawBall(mc);
+	mc.restore();
+}
+
+function drawMovingBall(mc, o) {
+	var m = mc.canvas;
+	mc.save();
+	mc.translate(m.width / 2, 100);
 	mc.rotate(o);
+	drawBall(mc);
+	mc.restore();
+}
+function drawBall(mc) {
 	mc.beginPath();
 	mc.moveTo(0, 0);
 	mc.arc(0, 0, 4, 0, 2 * Math.PI, false);
@@ -77,5 +103,4 @@ function something(mc) {
 	mc.closePath();
 	mc.stroke();
 	mc.fill();
-	mc.restore();
 }
